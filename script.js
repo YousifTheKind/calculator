a5ba70b2cac37cdddebf6cb65a9e51540e59ccb2
yousif <iikxt@yahoo.com>
2024-02-21 16:41:18 +0300

    Create operate function & variables for operation
    Made three variables for each part of the operation
    operator, firstNum, and secondNum.
    
    Created operate function that takes an operator
    and the first number and the second number
    and calls the appropriate function for the operation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+let operatorSign;
+let firstNum;
+let secondNum;
 
 const add = function(firstNum, secondNum) {
     return firstNum + secondNum;
@@ -16,4 +18,26 @@ const divide = function(firstNum, secondNum) {
     return (firstNum / secondNum).toFixed(2);
 }
 
-console.log(divide(9, 3));
+const operate = function(operator, firstNum, secondNum) {
+    let result;
+
+    switch(operator) {
+        case "+":
+            result = add(firstNum, secondNum);
+            break;
+        case "-":
+            result = subtract(firstNum, secondNum);
+            break;
+        case "*":
+            result = multiply(firstNum, secondNum);
+            break;
+        case "/":
+            result = divide(firstNum, secondNum);
+        default:
+            return "something went wrong!"
+    }
+
+    return result;
+}
+
+console.log(operate("+", 9, 3));
