9828c5191afabe79cb930e484c722872a8224bbb
yousif <iikxt@yahoo.com>
2024-02-21 15:55:13 +0300

    Create basic operations functions
    rounded subtraction answers to two decimal points

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+const add = function(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+const subtract = function(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+const multiply = function(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+const divide = function(firstNum, secondNum) {
+    return (firstNum / secondNum).toFixed(2);
+}
+
+console.log(divide(9, 3));
