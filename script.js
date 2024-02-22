ea7f6ef1ea3b9ff33fba936b9d9443428416bdb9
yousif <iikxt@yahoo.com>
2024-02-22 13:20:30 +0300

    Fix bugs when user clicks = and + multiple times

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let operatorSign;
 let firstNum;
 let secondNum;
 let displayValue;
-firstClick = true;
+let firstClick = true;
+let resultOnScreen = false;
 const display = document.querySelector("#display");
 const digits = document.querySelector("#digits");
 const operationButton = document.querySelector("#operationButton");
@@ -22,7 +23,7 @@ const multiply = function(firstNum, secondNum) {
 };
 
 const divide = function(firstNum, secondNum) {
-    return (firstNum / secondNum).toFixed(2);
+    return (firstNum / secondNum);
 };
 
 const operate = function(operator, firstNum, secondNum) {
@@ -40,27 +41,37 @@ const operate = function(operator, firstNum, secondNum) {
             break;
         case "/":
             result = divide(firstNum, secondNum);
+            break;
         default:
+            console.log("something went wrong!")
             return "something went wrong!"
     }
 
-    return result;
+    return Math.round(result * 100) / 100;
 };
 
 const populate = function(displayValue) {
-    display.textContent += displayValue;
+
+    if(displayValue == " ") {
+        display.textContent = " ";
+    }
+    else if(typeof displayValue === "string" || typeof displayValue === "number") {
+        display.textContent += displayValue
+    };
+
 };
 
 
 digits.addEventListener("click", (e) => {
+    if(resultOnScreen) {populate(" "); resultOnScreen = false}
     if(!firstClick) {
-        display.textContent = ""
         displayValue = e.target.getAttribute("value");
-        console.log("first number from !firstClick: " + firstNum)
+        populate(displayValue);
+    }
+    else {
+        displayValue = e.target.getAttribute("value");
+        populate(displayValue);
     };
-    displayValue = e.target.getAttribute("value");
-    if(typeof displayValue === "string") populate(displayValue);
-
 
 });
 operationButton.addEventListener("click", (e) => {
@@ -69,27 +80,28 @@ operationButton.addEventListener("click", (e) => {
         operatorSign = e.target.getAttribute("value")
         firstNum = Number(display.textContent);
         display.textContent = "";
-        console.log("first number: " + firstNum)
         firstClick = false;    
     }
     else {
         secondNum = Number(display.textContent);
-        display.textContent = "";
-        console.log("2nd number: " + secondNum);
+        populate(" ");
         let result = operate(operatorSign, firstNum, secondNum);
         populate(result);
         firstNum = result;
         operatorSign = e.target.getAttribute("value")
-        console.log(operatorSign);
     }
 });
 
 equal.addEventListener("click", () => {
-    secondNum = Number(display.textContent);
-    display.textContent = "";
-    console.log("2nd number: " + secondNum);
-    let result = operate(operatorSign, firstNum, secondNum);
-    populate(result);
+    console.log("first click: " + firstClick);
+    if(!firstClick) {
+        secondNum = Number(display.textContent);
+        populate(" ");
+        let result = operate(operatorSign, firstNum, secondNum);
+        populate(result);
+        firstClick = true;
+        resultOnScreen = true;
+    }
 });
 
 
