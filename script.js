3f77f559911d8c8d35392eb00b9d988ddf9e1751
yousif <iikxt@yahoo.com>
2024-02-22 11:48:08 +0300

    Wrote first draft logic for calc functionality
    User can enter a number then operation sign and
    another number then press equal and result will
    show.
    
    User can string together several operations

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ let operatorSign;
 let firstNum;
 let secondNum;
 let displayValue;
+firstClick = true;
 const display = document.querySelector("#display");
 const digits = document.querySelector("#digits");
+const operationButton = document.querySelector("#operationButton");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clearButton");
 
 const add = function(firstNum, secondNum) {
     return firstNum + secondNum;
-}
+};
 
 const subtract = function(firstNum, secondNum) {
     return firstNum - secondNum;
-}
+};
 
 const multiply = function(firstNum, secondNum) {
     return firstNum * secondNum;
-}
+};
 
 const divide = function(firstNum, secondNum) {
     return (firstNum / secondNum).toFixed(2);
-}
+};
 
 const operate = function(operator, firstNum, secondNum) {
     let result;
@@ -41,14 +45,57 @@ const operate = function(operator, firstNum, secondNum) {
     }
 
     return result;
-}
-
-digits.addEventListener("click", (e) => {
-    displayValue = e.target.getAttribute("value");
-    populate(displayValue)
-})
+};
 
 const populate = function(displayValue) {
-    display.textContent = displayValue;
-} 
+    display.textContent += displayValue;
+};
 
+
+digits.addEventListener("click", (e) => {
+    if(!firstClick) {
+        display.textContent = ""
+        displayValue = e.target.getAttribute("value");
+        console.log("first number from !firstClick: " + firstNum)
+    };
+    displayValue = e.target.getAttribute("value");
+    if(typeof displayValue === "string") populate(displayValue);
+
+
+});
+operationButton.addEventListener("click", (e) => {
+
+    if(firstClick){
+        operatorSign = e.target.getAttribute("value")
+        firstNum = Number(display.textContent);
+        display.textContent = "";
+        console.log("first number: " + firstNum)
+        firstClick = false;    
+    }
+    else {
+        secondNum = Number(display.textContent);
+        display.textContent = "";
+        console.log("2nd number: " + secondNum);
+        let result = operate(operatorSign, firstNum, secondNum);
+        populate(result);
+        firstNum = result;
+        operatorSign = e.target.getAttribute("value")
+        console.log(operatorSign);
+    }
+});
+
+equal.addEventListener("click", () => {
+    secondNum = Number(display.textContent);
+    display.textContent = "";
+    console.log("2nd number: " + secondNum);
+    let result = operate(operatorSign, firstNum, secondNum);
+    populate(result);
+});
+
+
+clear.addEventListener("click", () => {
+    display.textContent = "";
+    firstNum = null;
+    secondNum = null;
+    operatorSign = null;
+});
